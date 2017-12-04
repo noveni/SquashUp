2982f872f2f0577a67a22addd1850bc29b6d8fe1
noveni <nico.novello@gmail.com>
2017-12-04 23:24:20 +0100

    correct User model codebase

--- www/app/User.php
+++ www/app/User.php
@@ -37,27 +37,29 @@ class User extends Authenticatable
     */
     public function authorizeRoles($roles)
     {
-    if (is_array($roles)) {
-        return $this->hasAnyRole($roles) ||
-                abort(401, 'This action is unauthorized.');
-    }
-    return $this->hasRole($roles) ||
+        if (is_array($roles)) {
+            return $this->hasAnyRole($roles) ||
+                    abort(401, 'This action is unauthorized.');
+        }
+        return $this->hasRole($roles) ||
             abort(401, 'This action is unauthorized.');
     }
+
     /**
     * Check multiple roles
     * @param array $roles
     */
     public function hasAnyRole($roles)
     {
-    return null !== $this->roles()->whereIn(‘name’, $roles)->first();
+        return null !== $this->roles()->whereIn('name', $roles)->first();
     }
+
     /**
     * Check one role
     * @param string $role
     */
     public function hasRole($role)
     {
-    return null !== $this->roles()->where(‘name’, $role)->first();
+        return null !== $this->roles()->where('name', $role)->first();
     }
 }
